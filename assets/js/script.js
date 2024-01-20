b3418d6c7866493efac31eaead69c7d60c010839
stringsister <71358676+stringsister@users.noreply.github.com>
2024-01-20 09:30:38 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,20 +15,27 @@ document.addEventListener("DOMContentLoaded", function () {
     let reviewsHTML = '';
     reviews.forEach(function (review) {
       reviewsHTML += `
+
+
+
+
+
         <div class="review-bubble">
           <div class="review-header">
-            <span class="reviewer-name">${review.author_name}</span>
-            <span class="star-rating">&#9733; ${review.rating}</span>
+         
+            
           </div>
+
+
           <p class="review-text">${review.text}</p>
+<span class="star-rating"> &#9733;&#9733;&#9733;&#9733;&#9733;</span> <br>
+<span class="reviewer-name">- ${review.author_name} </span> 
         </div>`;
     });
 
     reviewsContainer.innerHTML = reviewsHTML;
   }
 
-  // Update reviews every 30 minutes (adjust as needed)
-  setInterval(getGoogleReviews, 1800000);
 
   // Call the function to fetch and display reviews on page load
   getGoogleReviews();
